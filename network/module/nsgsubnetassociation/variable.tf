0c443f3947dd7620305a0699c4d73c0451f4657a
mishraamitkumar644 <69366648+mishraamitkumar644@users.noreply.github.com>
2026-06-25 18:25:31 +0530

    Delete network directory

--- network/module/nsgsubnetassociation/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "nsgsubnetassociation" {
-  type = map(object({
-    subnet_name                 = string 
-    virtual_network_name        = string
-    network_security_group_name = string
-    resource_group_name         = string
-  }))
-}
--- network/module/resourcegroup/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "rg" {
-    description = "List of Resource Group"
-    type        = map(object({
-        rg_name  = string
-        location = string
-    }))
-
-}
