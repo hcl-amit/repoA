a6d4bfe01eba40ca8ab362adf5556a8f414fd4b3
Amit Mishra <mishraamitkumar644@gmail.com>
2026-06-30 09:43:48 +0530

    rg updates

--- resource-group/example/main.tf
+++ resource-group/example/main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_version = ">= 1.13.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=4.23.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
 module "resource_group" {
   source = "../"
 
